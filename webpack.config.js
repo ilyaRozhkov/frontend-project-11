f3df3efc9e64ce99270ca88f6cdb8d5d8b969241
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2025-06-11 17:17:59 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+export default {
   performance: {
     maxAssetSize: 500000,
     maxEntrypointSize: 500000,
